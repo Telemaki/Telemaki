f7e202c90c3db4ad97866fc3874635607db2ddb7
LeoDog896 <LeoDog896@hotmail.com>
2020-03-16 19:53:04 -0400

    quick changes

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const {app, BrowserWindow} = require("electron");
+const RPC = require('discord-rpc');
+
+const clientId = '606983186760728606';
+const clientSecret = process.env.SECRET;
+const scopes = ['rpc'];
+ 
+const client = new RPC.Client({ transport: 'ipc' });
+ 
+client.on('ready', () => {
+  console.log('Logged in as', client.user.username);
+});
+ 
+client.login({ clientId, clientSecret });
 
 function createWindow() {
 	// Create the browser window.
@@ -13,5 +26,9 @@ function createWindow() {
 	// and load the index.html of the app.
 	win.loadFile("app/login/index.html");
 }
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit()
+})
 
 app.whenReady().then(createWindow);
