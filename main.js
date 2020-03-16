0b7130d5464d50a3f0e71832f7de08d3a7ab0892
LeoDog896 <LeoDog896@hotmail.com>
2020-03-16 16:31:29 -0400

    base login

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const {app, BrowserWindow} = require("electron");
 function createWindow() {
 	// Create the browser window.
 	let win = new BrowserWindow({
-		width: 1200,
-		height: 1000,
+		width: 1600,
+		height: 1600 / 16 * 9,
 		webPreferences: {
 			nodeIntegration: true
 		}
 	});
 
 	// and load the index.html of the app.
-	win.loadFile("app/dashboard/index.html");
+	win.loadFile("app/login/index.html");
 }
 
 app.whenReady().then(createWindow);
