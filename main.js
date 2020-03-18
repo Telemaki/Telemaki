a9977e7be3c20b38914923e9f7369bf42a2c829f
LeoDog896 <LeoDog896@hotmail.com>
2020-03-17 23:56:28 -0400

    Formatting changes

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const RPC = require('discord-rpc');
 
-const scopes = ['rpc', 'rpc.api'];
-
 const client = new RPC.Client({ transport: 'ipc' });
 
 let win;
@@ -10,15 +8,16 @@ let win;
 app.allowRendererProcessReuse = true
 
 client.on("ready", () => {
-    console.log(client.user.username)
     win.webContents.send('username', client.user.username);
     win.loadFile('app/dashboard/index.html')
 })
 
-ipcMain.on("login", event => client.login({ clientId: "id", clientSecret: "secret" }));
+ipcMain.on("login",      event       => client.login({ clientId: "id", clientSecret: "secret" }));
 ipcMain.on("changeUrl", (event, url) => win.loadFile(url))
 
-function createWindow() {
+app.on('window-all-closed', () => process.platform !== 'darwin' && app.quit())
+
+app.whenReady().then(() => {
     win = new BrowserWindow({
         width: 1600,
         height: 900,
@@ -29,10 +28,4 @@ function createWindow() {
     });
     win.once('ready-to-show', () => win.show())
     win.loadFile("app/login/index.html");
-}
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-})
-
-app.whenReady().then(createWindow);
+});
